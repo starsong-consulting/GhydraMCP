a10bed092adf22fe4d614e668f51e3a41f4699a0
Teal Bauer <teal@starsong.eu>
2026-06-16 23:37:02 +0200

    chore: bump API_VERSION to 3000 (breaking: fully-qualified names)

--- src/main/java/eu/starsong/ghidra/api/ApiConstants.java
+++ src/main/java/eu/starsong/ghidra/api/ApiConstants.java
@@ -2,7 +2,7 @@ package eu.starsong.ghidra.api;
 
 public class ApiConstants {
     public static final String PLUGIN_VERSION = "v3.0.0-beta";
-    public static final int API_VERSION = 2020;
+    public static final int API_VERSION = 3000;
     public static final int DEFAULT_PORT = 8192;
     public static final int MAX_PORT_ATTEMPTS = 256;
 }
